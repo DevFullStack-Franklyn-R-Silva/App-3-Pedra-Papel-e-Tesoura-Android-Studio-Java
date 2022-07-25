2f28233b6c26fe8f7d646bf46c9e0173a081e2b8
Franklyn R <franklyn1998@live.com>
2022-07-25 19:08:10 -0300

    Escolhendo opção

--- app/src/main/java/com/hadesfranklyn/pedrapapeltesoura/MainActivity.java
+++ app/src/main/java/com/hadesfranklyn/pedrapapeltesoura/MainActivity.java
@@ -1,8 +1,9 @@
- package com.hadesfranklyn.pedrapapeltesoura;
-
-import androidx.appcompat.app.AppCompatActivity;
+package com.hadesfranklyn.pedrapapeltesoura;
 
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void selecionadoPedra(View view) {
+        this.opcaoSelecionada("Pedra");
+    }
+
+    public void selecionadoPapel(View view) {
+        this.opcaoSelecionada("Papel");
+    }
+
+    public void selecionadoTesoura(View view) {
+        this.opcaoSelecionada("Tesoura");
+    }
+
+    public void opcaoSelecionada(String opcaoSelecionada) {
+        System.out.println("Item clicado: " + opcaoSelecionada);
     }
 }
