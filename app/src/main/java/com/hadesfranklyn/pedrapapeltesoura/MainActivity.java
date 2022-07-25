72ffe9ebeb5c726aa25e842f00e1af88a1959956
Franklyn R <franklyn1998@live.com>
2022-07-25 19:19:51 -0300

    Gerar escolha aleatória

--- app/src/main/java/com/hadesfranklyn/pedrapapeltesoura/MainActivity.java
+++ app/src/main/java/com/hadesfranklyn/pedrapapeltesoura/MainActivity.java
@@ -2,8 +2,11 @@ package com.hadesfranklyn.pedrapapeltesoura;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +29,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void opcaoSelecionada(String opcaoSelecionada) {
-        System.out.println("Item clicado: " + opcaoSelecionada);
+
+        ImageView imageResultado = findViewById(R.id.imageResultado);
+
+        int numero = new Random().nextInt(3);// 0 1 2
+
+        String[] opcoes = {"Pedra", "Papel", "Tesoura"};
+        String opcaoApp = opcoes[numero];
+
+        switch (opcaoApp){
+            case "Pedra":
+                imageResultado.setImageResource(R.drawable.pedra);
+                break;
+            case "Papel":
+                imageResultado.setImageResource(R.drawable.papel);
+                break;
+            case "Tesoura":
+                imageResultado.setImageResource(R.drawable.tesoura);
+                break;
+        }
+
+
+        System.out.println("Item clicado: " + opcaoApp);
+
     }
 }
